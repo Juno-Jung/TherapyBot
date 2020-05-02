94844b4d2dfa0498e8f1950b0e7e6de09d34caaf
Juno <Laevino@gmail.com>
2020-05-02 10:24:21 -0400

    Setting up the framework of the ChatBot.

--- index.js
+++ index.js
@@ -3,3 +3,26 @@
 // Router
 
 // Controllers
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const router = express.Router();
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('tiny'));
+
+
+app.use(router);
+router.get('/', indexController);
+router.get('/message', messageController);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => {
+  console.log(`TherapyBot listening at http://localhost:${port}`)
+});
