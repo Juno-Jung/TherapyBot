bcaae4e87961e06ef996d3f8cbf10b59cc84427c
Juno <Laevino@gmail.com>
2020-07-10 10:24:27 -0400

    chore: Reorganized the files into a single server folder.

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-
-// Currently, we are only serving static assets for index.html
-const indexController = (req, res) => {
-};
-
-
-module.exports = indexController;
--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const indexController = require('./../controllers/index');
 const messageController = require('./../controllers/message');
 
 const router = express.Router();
 
-// indexController is unnecessary as the index.html is being served from the public folder.
-router.get('/', indexController);
 router.get('/message', messageController.getMessages);
 router.post('/message', messageController.postMessage);
 
